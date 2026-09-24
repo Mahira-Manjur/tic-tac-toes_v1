33f5d9cdb9fc944546388e0410b526fbf79a5982
Mahira Manjur <mahiramanjur007@gmail.com>
2026-09-24 21:42:15 +0600

    Game.c updated

--- game.c
+++ game.c
@@ -13,5 +13,61 @@ void startGme(int module){
     }
     else if(module==4){
         playerVsAI3(board);
+    } 
+}
+
+void playerVsPlayer(char board[3][3]){
+    int index= playerMove();
+    char c1='X';
+    validMovecheck(board,index,c1);
+    puttingInput(board,index,c1);
+}
+void playerVsAI1(char board[3][3]){
+    
+}
+void playerVsAI2(char board[3][3]){
+    
+}
+void playerVsAI3(char board[3][3]){
+    
+}
+int playerMove(){
+    int row,col;
+    printf("Enter row and column (1-3): ");
+    scanf("%d %d", &row, &col);
+    if(row<1 || row>3 || col<1 || col>3){
+        return playerMove();
+    }
+    return ((row-1)*3+col-1);
+}
+void validMovecheck(char board[3][3],int index,char token){
+    int cnt=1;
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            if(board[i][j]!=' '){
+                cnt++;
+            }
+        }
+    }// first e check kortese jei jaygay ami bari banabo oita earth er moddhe ase nki
+    if(cnt==9) return ;
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            if(index==((i*3)+j)){
+                if(board[i][j]!=' '){// yes jayga ta earth er moddhe but oi jaygay onno karo bari ase nki
+                    printf("Invalid move! Try again.\n");
+                    index= playerMove();
+                    validMovecheck(board,index,token);
+                }
+            }
+        }
+    }
+}
+
+void puttingInput(char board[3][3], int idx, char token){
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            if(idx==(i*3)+j)
+            board[i][j]=token;
+        }
     }
 }
